5a185b4ebd1e4c6a5701b576396e30249af8a713
amidpro <61383020+amidpro@users.noreply.github.com>
2022-01-31 03:53:22 +0000

    Exercise 8 - Variables
    An attempt to clean up code, whilst uploading homework set for week one of Get Into Tech Bootcamp

---
+++ say_my_name.py
@@ -0,0 +1,9 @@
+firstname = "Pri"
+lastname = "Adom"
+
+print(firstname, lastname)
+#adding a comma to seperate variables leaves space between the strings
+
+print(" ".join([firstname, lastname]))
+# this is another way I found out how to space 2 strings together, using .join, will ask what this means
+
